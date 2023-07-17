198300f5571de1e146c0700a0d4e82e204597a91
INDIHARSHA <110756813+INDIHARSHA@users.noreply.github.com>
2023-07-17 18:02:00 +0530

    Add files via upload

---
+++ Numerology.java
@@ -0,0 +1,124 @@
+import java.util.*;
+class Numerology{
+    
+ public static void main(String args[])
+ {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("PLEASE ENTER THE DATE OF BIRTH");
+     int n=sc.nextInt();
+     if(0<n && 32>n)
+     {
+      switch(n)
+      {
+         case 1:
+         System.out.println("According to numerology for birthday, a person born on this day will have a natural zeal to lead others.Self-confidence will play a big role in framing your personality.");
+         break;
+         case 2:
+         System.out.println("You will have a warm demeanor and you will have the inherent quality of making friends easily.But you will also have a very moody attitude and can easily get upset.");
+         break;
+         case 3:
+         System.out.println("According to the relationship of numerology with birthday, a person born on the 3rd of the month has a restless attitude.They are also good at striking conversations with others.");
+         break;
+         case 4:
+         System.out.println("You are a very hard-working individual who always wants to do good in life.You are always in control and you know how not to let your emotions have the better of you.");
+         break;
+         case 5:
+         System.out.println("Flexibility is your forte. When life throws you a curveball, you can easily adapt to new circumstances and find excitement in the unexpected change. This ability to turn on a dime gives you the power to jump on brief opportunities others may miss.");
+         break;
+         case 6:
+         System.out.println("If your date of birth is the 6th, then your heart is your gift. You are a natural-born nurturer and have a great talent for helping and healing others. You are the epitome of self-sacrifice and a fierce protector of those you love.");
+         break;
+         case 7:
+         System.out.println("You possess a very refined mind and a deep urge to uncover life's mysteries. Your ability to acquire vast knowledge on both the informational and spiritual planes gives you a greater awareness than most.");
+         break;
+         case 8:
+         System.out.println("With a Birthday number 8, yours is a story of success. Your talent for setting and reaching goals is like no other. You are self-sufficient and capable and hold a great amount of power to achieve your ambitions.");
+         break;
+         case 9:
+         System.out.println("It's your compassion that makes you shine. You're devoted to helping the greater good and have a strong talent for speaking up for others. Your soul is most satisfied when you are giving and being of service");
+         break;
+         case 10:
+         System.out.println("Your 10 Birthday number blesses you with great leadership skills. Your mind is sharp and allows you to dream up ingenious ideas and organize all the details, then direct others how to carry things out.");
+         break;
+         case 11:
+         System.out.println("You have a keen awareness of what's happening around you. A strong intuition is your gift and will help you understand the unspoken feelings, thoughts, and fears of others. This insight allows you to be a great guide and supporter.");
+         break;
+         case 12:
+         System.out.println("With the number 12 as your date of birth, creativity is a driving force in your life. Your imagination is rich and you are able to express your feelings and insights in unique ways that others can benefit from.");
+         break;
+         case 13:
+         System.out.println("Driven by the number 13, you are a conscientious worker with a knack for coming up with creative ideas and turning them into something real. An optimistic but practical outlook keeps you determined and on track as you work steadily toward your goals.");
+         break;
+         case 14:
+         System.out.println("You are open-minded and always up to try something new, yet you are wise enough to stop and think before you jump right into things. This pragmatic approach helps ensure your time, attention, and efforts are directed into meaningful experiences.");
+         break;
+         case 15:
+         System.out.println("If you were born on the 15th day of the month, your love for others is powerful and you are able to spread your gift of support far and wide. Your curious and social nature brings you into contact with a variety of people, all who would benefit from your heartfelt words of wisdom.");
+         break;
+         case 16:
+         System.out.println("Your 16 Birthday number blesses you with an inquisitive mind that allows you to uncover important truths. You have a special ability to read into other people's feelings and motives and share with them your wisdom.");
+         break;
+         case 17:
+         System.out.println("The quality of work you can produce when you're going it alone is almost unbelievable. You are as independent as you are ambitious, capable of performing every step along the way with efficiency, focus, and skill.");
+         break;
+         case 18:
+         System.out.println("You are both open-minded and open-hearted and your ambition is to leave this world better than you found it. You are independent by nature but will feel most fulfilled and successful when you are of service to others.");
+         break;
+         case 19:
+         System.out.println("With your 19 Birthday number, independence and self-sufficiency are necessities to you. You are extremely capable in life and work and aren't afraid to take big risks to achieve the life you desire.");
+         break;
+         case 20:
+         System.out.println("You relate to others on an almost cosmic level. You are dedicated to building cooperative, harmonious relationships of all kinds and can use your sensitive intuition to sense and address the needs of others.");
+         break;
+         case 21:
+         System.out.println("If your birthday is on the 21st, you thrive in active social settings and find great value in connecting with people. Your natural charm paired with your creative styles of thinking and communicating makes you an exciting and inspiring presence to those in your life");
+         break;
+         case 22:
+         System.out.println("A 22 Birthday number gives you the power to create great things. You are determined and hardworking and your ability to cooperate with others makes you an effective teammate or leader.");
+         break;
+         case 23:
+         System.out.println("You have a real zest for life and you're eager to experience anything and everything possible. You approach relationships and situations in an easygoing way and are a source of optimism and inspiration for others.");
+         break;
+         case 24:
+         System.out.println("With your Birthday number 24, you have a heart of gold and are very skilled at maintaining balanced, stable relationships. You are loyal to your loved ones and are able to be the nurturer, protector, and provider all at once.");
+         break;
+         case 25:
+         System.out.println("You have a great ability to take in and process information on both conscious and subconscious levels. Your curiosity is endless and your desire to dive deep into a variety of subjects will bring you an immense awareness of the world.");
+         break;
+         case 26:
+         System.out.println("If you were born on the 26th day of the month, you have a desire to succeed and will feel most accomplished when your work benefits others. Your intuitive awareness of what people want allows you to come up with innovative solutions to meet their needs.");
+         break;
+         case 27:
+         System.out.println("Your mind is wide open and you are tolerant and compassionate toward all ways of life. You are able to take in a vast amount of insights and information and apply this knowledge toward the greater good.");
+         break;
+         case 28:
+         System.out.println("In an effort to accomplish great things, you recognize the value of working with others. You make a capable and compassionate leader who can bring people together and drive the team toward achievement.");
+         break;
+         case 29:
+         System.out.println("With a 29 Birthday number, you have an amazing ability to bring things together. You receive powerful subconscious insights that help you clearly see how everything and everyone is connected.With a 29 Birthday number, you have an amazing ability to bring things together. You receive powerful subconscious insights that help you clearly see how everything and everyone is connected.");
+         break;
+         case 30:
+         System.out.println("If the date of your birth is the 30th, you are an original, innovative thinker and an excellent communicator. You have a great talent for using creativity to convey your ideas and your optimistic attitude has an uplifting effect on others.");
+         break;
+         case 31:
+         System.out.println("Your approach to life is an effective mix of both practicality and imagination. Your mind is stirring with creative ideas and you have the organizational skills to manifest them on the physical plane.");
+         break;
+        }
+    }
+     
+         else
+        
+        System.out.println("PLEASE ENTER NUMBER BETWEEN 1-31");  
+          
+       
+     
+     
+     
+     
+     
+     
+     
+     
+    }
+    
+}
